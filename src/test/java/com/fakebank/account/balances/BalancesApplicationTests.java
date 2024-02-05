32ad93863c1097cc4ca17a610c1003c580982574
Iago Machado <iagomach@msn.com>
2024-02-05 08:03:42 -0300

    Initial setup
    Create project structure, create API contract, configure openapi tools generator for the server

---
+++ src/test/java/com/fakebank/account/balances/BalancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakebank.account.balances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
