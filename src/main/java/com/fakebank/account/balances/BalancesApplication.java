c4caaba202bcea02e30378b2a83a1562ef79904e
Iago Machado <iagomach@msn.com>
2024-02-07 00:56:46 -0300

    Set enable feign client to application

--- src/main/java/com/fakebank/account/balances/BalancesApplication.java
+++ src/main/java/com/fakebank/account/balances/BalancesApplication.java
@@ -2,12 +2,14 @@ package com.fakebank.account.balances;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BalancesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BalancesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BalancesApplication.class, args);
+    }
 
 }
