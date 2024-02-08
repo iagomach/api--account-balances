f241d8d49bf411b0ba9bd450c7a86853f7f07ee9
Iago Machado <iagomach@msn.com>
2024-02-08 01:55:38 -0300

    Add update available limit to repository

--- src/main/java/com/fakebank/account/balances/repositories/CustomersAccountsRepository.java
+++ src/main/java/com/fakebank/account/balances/repositories/CustomersAccountsRepository.java
@@ -2,6 +2,10 @@ package com.fakebank.account.balances.repositories;
 
 import com.fakebank.account.balances.entities.accounts.CustomerAccount;
 
+import java.math.BigDecimal;
+
 public interface CustomersAccountsRepository {
     CustomerAccount findByName(String fullName);
+
+    void updateAvailableLimitByName(String fullName, BigDecimal amountToSum);
 }
